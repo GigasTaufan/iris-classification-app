ee726472af0c605bb0e2bd72c08a3ba95e3e7a29
gigastaufan <970196@telkom.co.id>
2022-09-04 14:33:03 +0700

    Classification

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ from sklearn.ensemble import RandomForestClassifier
 # SET PAGE CONFIG
 st.set_page_config(page_title="Iris Classification", page_icon=":bouquet:", layout="wide")
 
+# THE TITLE
+st.title(":bouquet: Iris Flower Classification")
+
 st.write("""
-    # Simple Iris Flower Prediction App
+    ### Simple Iris Flower Prediction App
 
     This web app predict the Iris flower type
 """)
@@ -18,7 +21,7 @@ st.write("""
 ## User can input the parameter here
 st.sidebar.header('User Input Parameter')
 
-# Function to collect the parameter 
+## Function to collect the parameter 
 def user_input_feature():
     sepal_length = st.sidebar.slider('Sepal Lenght', 4.3, 7.9, 5.4)
     sepal_width = st.sidebar.slider('Sepal Width', 2.0, 4.4, 3.4)
@@ -35,5 +38,66 @@ def user_input_feature():
     features = pd.DataFrame(data, index=[0])
 
     return features
+
+## Function for prediction
+def prediction_type(prediction):
+    st.write("""
+        <style>
+        .big-font {
+            font-weight:bold !important;
+            color: #F675A8;
+            font-size: 25px;
+        }
+        </style>
+    """, unsafe_allow_html=True)
+
+
+    if prediction == 'setosa':
+        st.write('<p class="big-font">Setosa</p>', unsafe_allow_html=True)
+    elif prediction == 'versicolor':
+        st.write('<p class="big-font">Versicolor</p>', unsafe_allow_html=True)
+    else:
+        st.write('<p class="big-font">Virginica</p>', unsafe_allow_html=True)
+
+
+# MAINPAGE
+## Call the user_input_feature to collect the parameters into a dataframe
+df = user_input_feature()
+
+## Show the values of the the parameters
+st.subheader('User Input Parameters')
+st.write(df)
+
+# LOAD THE IRIS DATASET
+iris = datasets.load_iris()
+x = iris.data
+y = iris.target
+
+## Call the Random Forest Classification Method
+clf = RandomForestClassifier()
+## Train the model
+clf.fit(x, y)
+## Predict the clasification from the parameters values
+prediction = clf.predict(df)
+## The probability of the classification
+prediction_probability = clf.predict_proba(df)
+
+
+# SHOW THE PREDICTION AND THE PROBABILITY OF THE PREDICTION
+st.subheader("### Class labels and their corresponding index number")
+st.write(iris.target_names)
+
+st.subheader('### Prediction')
+prediction_type(iris.target_names[prediction])
+
+st.subheader("### Prediction Probability")
+st.write(prediction_probability)
+
+
+
+
+
+
+
 
 
